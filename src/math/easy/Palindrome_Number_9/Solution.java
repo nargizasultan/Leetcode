7cb930cbeea0ddf0fe3bc679de279c8bb6366146
nargiza <sultanmuratova500@gmail.com>
2022-12-07 04:26:19 +0600

    polindrom number

---
+++ src/math/easy/Palindrome_Number_9/Solution.java
@@ -0,0 +1,29 @@
+package math.easy.Palindrome_Number_9;
+
+public class Solution {
+    public static void main(String[] args) {
+        int x=-121;
+        System.out.println(isPalindrome(x));
+
+    }
+    public static boolean isPalindrome(int x) {
+
+        char a;
+        char b;
+
+        String s = Integer.toString(x);
+        char[] chars = s.toCharArray();
+        int j= chars.length-1;
+
+        for (int i = 0; i < chars.length; i++, j--) {
+            a=chars[i];
+            b=chars[j];
+            if(!Character.getName(a).equals(Character.getName(b))){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+}
