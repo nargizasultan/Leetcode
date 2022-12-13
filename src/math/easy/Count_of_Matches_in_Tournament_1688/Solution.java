3accea184eb4c1a9aceb0bf1b210e076f30d9714
nargiza <sultanmuratova500@gmail.com>
2022-12-13 06:02:44 +0600

    ,

---
+++ src/math/easy/Count_of_Matches_in_Tournament_1688/Solution.java
@@ -0,0 +1,12 @@
+package math.easy.Count_of_Matches_in_Tournament_1688;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(numberOfMatches(3));
+    }
+
+    public static int numberOfMatches(int n) {
+        return n-1;
+
+    }
+}
