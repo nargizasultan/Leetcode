b6d9a740f6f0f2c8a9190174c934b1c613b21292
nargiza <sultanmuratova500@gmail.com>
2023-01-12 19:43:10 +0600

    Time: 1 ms (23.21%), Space: 40.8 MB (35.11%)

---
+++ src/Binaryz_Tree_Preorder_Traversal_144/Solution.java
@@ -0,0 +1,52 @@
+package Binaryz_Tree_Preorder_Traversal_144;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+        //  System.out.println(preorderTraversal(root));
+        System.out.println(preorderTraversal1(root));
+
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) return list;
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+
+            TreeNode pop = stack.pop();
+            list.add(pop.val);
+            if (pop.right != null) {
+                stack.add(pop.right);
+            }
+            if (pop.left != null) {
+                stack.add(pop.left);
+            }
+        }
+        return list;
+
+    }
+}
