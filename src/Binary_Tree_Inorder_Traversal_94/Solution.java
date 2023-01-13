8bd20c38b630488ee5d60a9173dd77dbaa89e9b4
nargiza <sultanmuratova500@gmail.com>
2023-01-13 14:24:37 +0600

    Time: 1 ms (35.91%), Space: 40.6 MB (73.23%)

---
+++ src/Binary_Tree_Inorder_Traversal_94/Solution.java
@@ -0,0 +1,52 @@
+package Binary_Tree_Inorder_Traversal_94;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+        System.out.println(inorderTraversal(root));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer>list=new ArrayList<>();
+        Stack<TreeNode>stack=new Stack<>();
+        if(root==null){
+            return list;
+
+        }
+        TreeNode cur=root;
+        while(!stack.isEmpty() || cur!=null){
+            while(cur!=null){
+                stack.add(cur);
+                cur=cur.left;
+            }
+            cur=stack.pop();
+            list.add(cur.val);
+            cur=cur.right;
+        }
+
+        return list;
+
+    }
+}
