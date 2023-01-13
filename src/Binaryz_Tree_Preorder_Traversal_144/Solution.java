5aa47b30829b2a2c4282d6d34b2bbddec7709367
nargiza <sultanmuratova500@gmail.com>
2023-01-13 14:53:43 +0600

    Time: 0 ms (100%), Space: 40.2 MB (93.8%)

--- src/Binaryz_Tree_Preorder_Traversal_144/Solution.java
+++ src/Binaryz_Tree_Preorder_Traversal_144/Solution.java
@@ -1,6 +1,7 @@
 package Binaryz_Tree_Preorder_Traversal_144;
 
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
@@ -9,6 +10,7 @@ public class Solution {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
         System.out.println(preorderTraversal(root));
+        System.out.println(preorderTraversalRecursively(root));
 
 
     }
@@ -48,5 +50,17 @@ public class Solution {
         }
         return list;
 
+    }
+    public static List<Integer> preorderTraversalRecursively(TreeNode root) {
+       return dfs(root, new ArrayList<>());
+
+    }
+    private static List<Integer>dfs(TreeNode root, List<Integer>list){
+        if(root==null){
+            return list;
+        }
+        list.add(root.val);
+        list=dfs(root.left, list);
+        return dfs(root.right, list);
     }
 }
