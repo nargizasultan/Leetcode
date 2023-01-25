58aa90c38d80cf5d634f75cdf6d7cc81f76ee4e8
nargiza <sultanmuratova500@gmail.com>
2023-01-25 16:40:31 +0600

    Time: 0 ms (100%), Space: 40 MB (80.30%)

---
+++ src/Sum_of_Left_Leaves_404/Solution.java
@@ -0,0 +1,42 @@
+package Sum_of_Left_Leaves_404;
+
+public class Solution {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        System.out.println(sumOfLeftLeaves(root));
+
+    }
+
+    public static int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+
+        } else if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }else{
+           return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+        }
+
+
+
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
